2b82b52d97e7c782611a567fd8568d73a7dc5efc
pkaramol <pkaramo@cti.gr>
2016-07-30 12:45:37 +0300

    removed pgk-resources from requirements.txt

--- the_site/settings.py
+++ the_site/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
-from storages.backends.s3boto import S3BotoStorage
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
